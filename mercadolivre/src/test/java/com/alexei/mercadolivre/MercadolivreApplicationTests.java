96d640a3aa6c2048f605bf0518ebe618002f4d68
Alexeiabianna <alexei.abianna@edu.pucrs.br>
2021-07-08 10:32:57 -0300

    Início do projeto

---
+++ mercadolivre/src/test/java/com/alexei/mercadolivre/MercadolivreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexei.mercadolivre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadolivreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
