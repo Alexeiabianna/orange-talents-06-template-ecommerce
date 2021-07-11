46fcf477c5b034b24ed8034518779aa645cb566e
Alexeiabianna <alexei.abianna@edu.pucrs.br>
2021-07-10 21:57:28 -0300

    Cadastro categoria validacao nome único

--- mercadolivre/src/main/java/com/alexei/mercadolivre/controller/form/CategoriaForm.java
+++ mercadolivre/src/main/java/com/alexei/mercadolivre/controller/form/CategoriaForm.java
@@ -2,11 +2,13 @@ package com.alexei.mercadolivre.controller.form;
 
 import javax.validation.constraints.NotBlank;
 
+import com.alexei.mercadolivre.config.validation.UniqueValue;
 import com.alexei.mercadolivre.models.Categoria;
 
 public class CategoriaForm {
 
     @NotBlank
+    @UniqueValue(domainClass = Categoria.class, fieldName = "nome")
     private String nome;
     private Long idCategoriaMae;
 
